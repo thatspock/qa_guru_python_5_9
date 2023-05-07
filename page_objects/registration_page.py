fe8bb6738987f5dabaeb865104d5dc7c767862b0
Some Name <thatspock@gmail.com>
2023-05-07 17:07:58 +0700

    add folders and refactor code

--- page_objects/registration_page.py
+++ page_objects/registration_page.py
@@ -1,8 +1,9 @@
-from selene import browser, have, be
 import os
 
+from selene import browser, have
 
-class RegistrationPage():
+
+class RegistrationPage:
     def open(self):
         browser.open('/automation-practice-form')
 
@@ -47,39 +48,13 @@ class RegistrationPage():
     def submit_form(self):
         browser.execute_script('document.getElementById("submit").click()')
 
-    def should_have_form_submission_text(self, value):
-        browser.element('#example-modal-sizes-title-lg').should(have.text(value))
+    @property
+    def form_submission(self):
+        return browser.element('#example-modal-sizes-title-lg')
 
-    def should_have_registration_data(self, *values):
-        browser.all('tbody tr').should(
-            have.exact_texts(*values))
+    @property
+    def user_data(self):
+        return browser.all('tbody tr')
 
     def close_submission_form(self):
         browser.element('#closeLargeModal').click()
-
-
-def test_homework():
-    registration_page = RegistrationPage()
-    registration_page.open()
-    # WHEN
-    registration_page.fill_in_first_name('Mr')
-    registration_page.fill_in_last_name('Spock')
-    registration_page.fill_in_email('mrspock@enterprise.com')
-    registration_page.pick_gender('Male')
-    registration_page.fill_in_phone_number('1800666553')
-    registration_page.fill_in_date_of_birth('13', 'May', '1985')
-    registration_page.fill_in_subjects('English')
-    registration_page.pick_hobby('Sports')
-    registration_page.upload_picture_file('test.jpg')
-    registration_page.fill_in_address('Enterprise (NCC-1701)')
-    registration_page.select_state('NCR')
-    registration_page.select_city('Delhi')
-    registration_page.submit_form()
-    registration_page.should_have_form_submission_text('Thanks for submitting the form')
-    # THEN
-    registration_page.should_have_registration_data(
-        'Student Name Mr Spock', 'Student Email mrspock@enterprise.com', 'Gender Male',
-        'Mobile 1800666553', 'Date of Birth 13 May,1985', 'Subjects English', 'Hobbies Sports',
-        'Picture test.jpg', 'Address Enterprise (NCC-1701)', 'State and City NCR Delhi'
-    )
-    registration_page.close_submission_form()
