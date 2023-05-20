3e263cbd1044a971480e33efbc819beb09500536
Some Name <thatspock@gmail.com>
2023-05-21 01:55:14 +0700

    delete browser configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,8 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    # browser.driver.maximize_window()
-    driver = webdriver.Chrome('drivers')
-    driver.set_window_size(1920, 1200)
-    browser.config.driver = lambda: driver
+    browser.driver.set_window_size(1920, 1200)
     browser.config.base_url = 'https://demoqa.com'
