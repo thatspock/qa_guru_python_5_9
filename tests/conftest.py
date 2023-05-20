f836c9b0d44a20e03ea8b85c04ce316c75f341e1
Some Name <thatspock@gmail.com>
2023-05-21 01:10:40 +0700

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     # browser.driver.maximize_window()
-    driver = webdriver.Chrome('./drivers/chromedriver')
-    driver.set_window_size(1920, 1200)
-    browser.config.driver = lambda: driver
+    # driver = webdriver.Chrome('./drivers/chromedriver')
+    # driver.set_window_size(1920, 1200)
+    # browser.config.driver = lambda: driver
     browser.config.base_url = 'https://demoqa.com'
