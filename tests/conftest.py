370e8162037ecf17ccc5d1283e5cae9628fe98ae
Some Name <thatspock@gmail.com>
2023-05-21 01:05:20 +0700

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     # browser.driver.maximize_window()
-    # browser.driver.set_window_size(1920, 1200)
+    driver = webdriver.Chrome('./drivers/chromedriver')
+    driver.set_window_size(1920, 1200)
+    browser.config.driver = lambda: driver
     browser.config.base_url = 'https://demoqa.com'
