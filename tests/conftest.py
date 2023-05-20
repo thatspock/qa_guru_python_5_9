8aad7a245db38c6ee52a46c420ab69289b6adfa6
Some Name <thatspock@gmail.com>
2023-05-21 01:21:04 +0700

    add os.path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from selene import browser
 from selenium import webdriver
-
+import os
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     # browser.driver.maximize_window()
-    driver = webdriver.Chrome('./drivers/chromedriver')
+    chromedriver_path = os.path.abspath('drivers/chromedriver')
+    driver = webdriver.Chrome(chromedriver_path)
     driver.set_window_size(1920, 1200)
     browser.config.driver = lambda: driver
     browser.config.base_url = 'https://demoqa.com'
