76a387210fb4331ec3864439bfa184dc3d73384d
Some Name <thatspock@gmail.com>
2023-05-21 17:07:06 +0700

    add selenoid code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,28 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     browser.driver.set_window_size(1920, 1200)
     browser.config.base_url = 'https://demoqa.com'
--- utils/attachment.py
+++ utils/attachment.py
@@ -11,14 +11,17 @@ class AllureAttachmentManager:
         self.add_logs()
         self.add_html()
 
+    @allure.step('Adding screenshots')
     def add_screenshot(self):
         png = self.browser.driver.get_screenshot_as_png()
         allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
+    @allure.step('Adding logs')
     def add_logs(self):
         log = "".join(f'{text}\n' for text in self.browser.driver.get_log(log_type='browser'))
         allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
+    @allure.step('Adding HTML page')
     def add_html(self):
         html = self.browser.driver.page_source
         allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
