bc1a27c9f9566db1055e714a593aaee0de0a2437
Some Name <thatspock@gmail.com>
2023-05-22 23:04:00 +0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,33 +8,29 @@ from utils.attachment import AllureAttachmentManager
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    def build_browser():
-        options = Options()
-        selenoid_capabilities = {
-            "browserName": "chrome",
-            "browserVersion": "100.0",
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": True
-            }
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
         }
+    }
 
-        options.capabilities.update(selenoid_capabilities)
+    options.capabilities.update(selenoid_capabilities)
 
-        driver = webdriver.Remote(
-            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-            options=options)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
-        browser = Browser(Config(driver))
-        yield browser
+    browser = Browser(Config(driver))
+    yield browser
 
-        allure_attachments = AllureAttachmentManager(browser)
-        allure_attachments.gather_all_attachments()
+    allure_attachments = AllureAttachmentManager(browser)
+    allure_attachments.gather_all_attachments()
 
-        return browser
+# browser.config.driver = driver
 
-    yield build_browser
-    # browser.config.driver = driver
-
-    # browser.driver.set_window_size(1920, 1200)
-    # browser.config.base_url = 'https://demoqa.com'
+# browser.driver.set_window_size(1920, 1200)
+# browser.config.base_url = 'https://demoqa.com'
